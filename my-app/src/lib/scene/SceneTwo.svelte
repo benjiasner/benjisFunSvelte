11bdc1e1628a02a9fb471e44e8a14d9064ebbc16
benjiasner <54417348+benjiasner@users.noreply.github.com>
2024-08-19 18:19:42 -0500

    Update SceneTwo.svelte

--- my-app/src/lib/scene/SceneTwo.svelte
+++ my-app/src/lib/scene/SceneTwo.svelte
@@ -1,27 +1,56 @@
 <script>
-    import { T } from '@threlte/core';
+    import { T, useFrame } from '@threlte/core';
     import { OrbitControls, Sky, interactivity } from '@threlte/extras';
 
     interactivity();
 
     let orientationViewToggle = false;
-    $: orientX = 0;
-    $: orientY = 0;
+    let clickCounter = 0;
+    $: targetX = 0;
+    $: targetY = 0;
+    $: targetZ = 0;
+    let orientX = 0;
+    let orientY = 0;
+    let orientZ = 0;
+    const rotationSpeed = 0.05; // Adjust for desired smoothness
+
+    function resetAxes() {
+        // Reset the target rotation to the current rotation
+        targetX = orientX;
+        targetY = orientY;
+        targetZ = orientZ;
+    }
 
     function toggleOrientationView() {
         orientationViewToggle = !orientationViewToggle;
-        console.log('orientation view toggle clicked')
+        clickCounter += 1;
+        console.log('orientation view toggle clicked number of times ' + clickCounter);
     }
 
     function rotateX(){
-        orientX = orientX + Math.PI / 2;
+        resetAxes();
+        targetX += Math.PI / 2;
         console.log('orientX')
     }
 
     function rotateY(){
-        orientY = orientY + Math.PI / 2;
+        resetAxes();
+        targetY += Math.PI / 2;
         console.log('orientY')
     }
+
+    function rotateZ(){
+        resetAxes();
+        targetZ += Math.PI / 2;
+        console.log('orientZ')
+    }
+
+    useFrame(() => {
+        // Smoothly interpolate rotation values towards the target
+        orientX += (targetX - orientX) * rotationSpeed;
+        orientY += (targetY - orientY) * rotationSpeed;
+        orientZ += (targetZ - orientZ) * rotationSpeed;
+    });
 </script>
 
 <T.PerspectiveCamera makeDefault position={[10, 10, 10]} zoom={2} fov={80}>
@@ -30,23 +59,23 @@
 
 <Sky />
 
-<T.Mesh position={[0, 1, 0]} rotation={[orientY, 0, orientX]} on:click={() => {toggleOrientationView()}}>
+<T.Mesh position={[0, 1, 0]} rotation={[orientX, orientY, orientZ]} on:click={() => {toggleOrientationView()}}>
     <T.TorusKnotGeometry />
     <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
 </T.Mesh>
 
 {#if orientationViewToggle}
-    <T.Mesh position={[0, 5, 0]} rotation={[Math.PI / 2, 0, 0]} on:click={() => {console.log('arrow clicked z')}}>
+    <T.Mesh position={[0, 5, 0]} rotation={[Math.PI / 2, 0, 0]} on:click={() => {rotateX()}}>
         <T.ConeGeometry args={[.2, 1, 40]}/>
         <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
     </T.Mesh>
 
-    <T.Mesh position={[0, 0, 5]} rotation={[Math.PI / 2, 0, - Math.PI / 2]} on:click={() => {rotateX()}}>
+    <T.Mesh position={[0, 0, 5]} rotation={[Math.PI / 2, 0, - Math.PI / 2]} on:click={() => {rotateY()}}>
         <T.ConeGeometry args={[.2, 1, 40]}/>
         <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
     </T.Mesh>
 
-    <T.Mesh position={[5, 0, 0]} rotation={[Math.PI, 0, Math.PI]} on:click={() => {rotateY()}}>
+    <T.Mesh position={[5, 0, 0]} rotation={[Math.PI, 0, Math.PI]} on:click={() => {rotateZ()}}>
         <T.ConeGeometry args={[.2, 1, 40]}/>
         <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
     </T.Mesh>
