cd5838eb5bc63a14375f4aa8854d8f4c29b8e6bb
benjiasner <54417348+benjiasner@users.noreply.github.com>
2024-08-19 14:32:26 -0500

    getting close

--- my-app/src/lib/scene/SceneTwo.svelte
+++ my-app/src/lib/scene/SceneTwo.svelte
@@ -1,6 +1,27 @@
 <script>
     import { T } from '@threlte/core';
-    import { OrbitControls, Sky } from '@threlte/extras';
+    import { OrbitControls, Sky, interactivity } from '@threlte/extras';
+
+    interactivity();
+
+    let orientationViewToggle = false;
+    $: orientX = 0;
+    $: orientY = 0;
+
+    function toggleOrientationView() {
+        orientationViewToggle = !orientationViewToggle;
+        console.log('orientation view toggle clicked')
+    }
+
+    function rotateX(){
+        orientX = orientX + Math.PI / 2;
+        console.log('orientX')
+    }
+
+    function rotateY(){
+        orientY = orientY + Math.PI / 2;
+        console.log('orientY')
+    }
 </script>
 
 <T.PerspectiveCamera makeDefault position={[10, 10, 10]} zoom={2} fov={80}>
@@ -9,9 +30,27 @@
 
 <Sky />
 
-<T.Mesh position={[0, 1, 0]} >
-    <T.TorusKnotGeometry/>
+<T.Mesh position={[0, 1, 0]} rotation={[orientY, 0, orientX]} on:click={() => {toggleOrientationView()}}>
+    <T.TorusKnotGeometry />
     <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
 </T.Mesh>
+
+{#if orientationViewToggle}
+    <T.Mesh position={[0, 5, 0]} rotation={[Math.PI / 2, 0, 0]} on:click={() => {console.log('arrow clicked z')}}>
+        <T.ConeGeometry args={[.2, 1, 40]}/>
+        <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
+    </T.Mesh>
+
+    <T.Mesh position={[0, 0, 5]} rotation={[Math.PI / 2, 0, - Math.PI / 2]} on:click={() => {rotateX()}}>
+        <T.ConeGeometry args={[.2, 1, 40]}/>
+        <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
+    </T.Mesh>
+
+    <T.Mesh position={[5, 0, 0]} rotation={[Math.PI, 0, Math.PI]} on:click={() => {rotateY()}}>
+        <T.ConeGeometry args={[.2, 1, 40]}/>
+        <T.MeshStandardMaterial roughness={0} color={'gray'} metalness={'1'}/>
+    </T.Mesh>
+{/if}
+
 
 <T.GridHelper />
